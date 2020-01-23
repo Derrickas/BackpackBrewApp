d4ee33d024db55b0def90d8263a36d4fc32ae471
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-01-23 12:15:54 -0600

    rounded temp and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
-    var weatherCalls = "https://api.weatherbit.io/v2.0/subscription/usage?key=" + weatherAPI
+
     var weatherAPI = "d6e8ffe90ebe474fa1ad37347201fc11"
+    var cityArr = [];
 
 
+// when user types a city the the weather conditions show
 $("#citySearch").on("click", function(event) {
     event.preventDefault();
     var city = $("#city-input").val().trim();
 
     localWeather(city)
     forecast(city)
+    saveCitySearch(city)
 })
 
+
+
+// Elements that makeup current weather conditions
     function localWeather(city) {
         var queryURL = "https://api.weatherbit.io/v2.0/current?city=" + city + "&key=" + weatherAPI;
 
@@ -20,21 +26,26 @@ $("#citySearch").on("click", function(event) {
     }).then(function(response){
         console.log(response)
 
+        // pulling their icons based on the weather conditions
         var showIcon = "https://www.weatherbit.io/static/img/icons/" + response.data[0].weather.icon + ".png";
+        // converting the temperature from celsius to fahrenheit
+        var temperature = response.data[0].temp * 9/5 + 32.
+        // rounding the temperature to get rid of decimals
+        var conTemp = temperature.toFixed(0)
+
         var cityName = $("#cityTitle").text(response.data[0].city_name + ", " + response.data[0].state_code + " " + "(" + moment().format("MM-DD-YYYY") + ")");
             $("#currentWeather").append(cityName);
         var cityIcon = $("#cityIcon").html('<img src="' + showIcon + '"/>');
             $("#currentWeather").append(cityIcon);
         var description = $("#description").text("Conditions: " + response.data[0].weather.description)
             $("#currentWeather").append(description);
-        var temp = $("#temp").text("Temperature: " + response.data[0].temp + " °F")
-            $("#currentWeather").append(temp);
-        
-    })
+        var temp = $("#temp").text("Temperature: " + conTemp + " °F")
+            $("#currentWeather").append(temp);  
+        })
     }
 
 
-    
+// element that predicts tomorrows forecast 
     function forecast(city) {
         var queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + city + "&key=" + weatherAPI;
 
@@ -44,10 +55,20 @@ $("#citySearch").on("click", function(event) {
     }).then(function(response){
         console.log(response)
 
+        // getting tomorrows weather forecast description 
         var tomDescrip = $("#tomDescrip").text("Tomorrow's forecast: " + response.data[1].weather.description)
             $("#currentWeather").append(tomDescrip);
-    })
+        })
     }
+
+
+    
+
+// saving to local storage 
+function saveCitySearch(city) {
+    cityArr.push(city.toLowerCase());
+    localStorage.setItem("city", JSON.stringify(cityArr));
+}
 
 
 
