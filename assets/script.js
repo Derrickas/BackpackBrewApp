65e797bcdc8acdf7f3383e4f0985ddc90df1ee5c
Christopher Wood <cjwood01@gmail.com>
2020-01-22 20:30:36 -0600

    fixed search functionality to properly display search input.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
-var API_KEY = '14992449-1f5a79fc7605f2a9694e87a5b';
-var queryURL = "https://pixabay.com/api/?key="+ API_KEY;
+function searchCitiesInTown(city) {
+    var API_KEY = '14992449-1f5a79fc7605f2a9694e87a5b';
+    
+    var queryURL = "https://pixabay.com/api/?key="+ API_KEY +"&q="+ city;
 
-
- // Event listener for our button
- $(".button").on("click", function() {
 
 // Perfoming an AJAX GET request to our queryURL
 $.ajax({
@@ -16,17 +15,27 @@ console.log(queryURL);
 console.log(response);
 console.log(response.hits[0].largeImageURL)
 
-
-
-
 // Creating and storing an image tag
 var image = $("<img>");
 
-// Setting the image src attribute to imageUrl
+// Setting the image src attribute to largeImageUrl
   image.attr("src", response.hits[0].largeImageURL);
   image.attr("alt", "city image");
-
+  $("#imageCity").empty();
   $("#imageCity").prepend(image);
+});
+}
+
+// Event listener for our button
+$(".button").on("click", function(event) {
+    // Preventing the button from trying to submit the form
+     event.preventDefault();
+    // Storing the city name
+    var inputCities = $(".input").val().trim();
+  // Running the searchCitiesInTown function (passing in the city as an argument)
+  searchCitiesInTown(inputCities);
 
 });
-});
+   
+
+  
