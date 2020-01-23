57cde13a9defc3b2c48b7776738cde87c7153a1a
Christopher Wood <cjwood01@gmail.com>
2020-01-22 18:40:16 -0600

    Created an img tag and set it to contain the image url from Pixabay. Prepended to imageCity div in HTML

--- assets/script.js
+++ assets/script.js
@@ -2,30 +2,31 @@ var API_KEY = '14992449-1f5a79fc7605f2a9694e87a5b';
 var queryURL = "https://pixabay.com/api/?key="+ API_KEY;
 
 
- // Event listener for our cat-button
-//  $("#cat-button").on("click", function() {
+ // Event listener for our button
+ $(".button").on("click", function() {
 
- // Storing our giphy API URL for a random cat image
-
-  
 // Perfoming an AJAX GET request to our queryURL
 $.ajax({
     url: queryURL,
     method: "GET"
         })
-  .then(function(response) {
+
+    .then(function(response) {
 console.log(queryURL);
 console.log(response);
 console.log(response.hits[0].largeImageURL)
 
+
+
+
+// Creating and storing an image tag
+var image = $("<img>");
+
+// Setting the image src attribute to imageUrl
+  image.attr("src", response.hits[0].largeImageURL);
+  image.attr("alt", "city image");
+
+  $("#imageCity").prepend(image);
+
 });
-
-
-// // Retrieving the URL for the image
-// var imgURL = response.Poster;
-
-// // Creating an element to hold the image
-// var image = $("<img>").attr("src", imgURL);
-
-// // Appending the image
-// movieDiv.append(image);
+});
